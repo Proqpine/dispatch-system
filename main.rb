18feb372d3777f9b336ddae03d21cfceeeacc73d
Ayomided <adediji.d@yahoo.com>
2024-09-24 22:33:26 +0100

    travelling in the elevator, logic made on sleepy brain

--- main.rb
+++ main.rb
@@ -13,20 +13,24 @@ class Elevator
   end
 
   def call_elevator
-    to_go = @position + @call_floor
-    return if to_go > @destination
+    to_go = if @position > @call_floor # 0 > 5?
+              @position - @call_floor
+            else # 0 < 5
+              @position + @call_floor
+            end
+    puts "Elevator on #{@position}"
+    return if to_go > @floors
 
     @position = to_go
-    puts "Called on #{to_go}"
+    puts "Called to #{@destination} from #{@call_floor}. Elevator on #{@position}"
+    travel
   end
 
-  private
-
-  def elevator_position
-    @position
+  def travel
+    puts "Going #{@position - @destination} floors to destination"
   end
 end
 
-gs_elevator = Elevator.new(2)
-gs_elevator.select_floor(1, 1)
+gs_elevator = Elevator.new(10)
+gs_elevator.select_floor(1, 5)
 gs_elevator.call_elevator
